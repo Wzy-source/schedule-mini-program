cebe3e173bede4bb227485a7702402e450722edc
茫茫燃 <191830154@smail.nju.edu.cn>
2021-08-31 01:11:05 +0800

    完成消息提醒部分

---
+++ test/tab滑动切换/pages/index/cell1.vue
@@ -0,0 +1,30 @@
+<template>
+  <view @click="pressCell()" class="" style="display: flex;flex-direction: row;align-items: center;justify-content: center; background-color: #A0CFFF;width: 100%;height: 100%;">
+	{{itemData.title}}
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      itemData: {
+        type: Object,
+        default: function(e) {
+          return {}
+        }
+      }
+    },
+    methods: {
+      pressCell(){
+		  uni.showModal({
+		  	title:"点击",
+		  })
+	  }
+	  
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
